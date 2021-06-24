50e3cf2ab3925fc7c25e5914031301e55b52f424
Bryce Greene <brycegreene20@gmail.com>
2021-06-24 12:45:45 -0400

    1st commit for resolving AWS Elastic Beanstalk Errors

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 (async () => {
 
   // Init the Express application
@@ -8,28 +8,25 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get('/filteredimage/', async (req, res) =>{
-    
-    let {image_url}= req.query;
-    let image =  await filterImageFromURL(image_url);
+  app.get('/filteredimage/', async (req, res) => {
+    let { image_url } = req.query;
+    let image = await filterImageFromURL(image_url);
 
-    if (image_url){
-        console.log("Query worked");
-
-        filterImageFromURL(image_url).then((response) =>{
-          res.sendFile(response);
-          res.on('finish', function(){
-            deleteLocalFiles([response]);
-          });
+    if (image_url) {
+      console.log("Query worked");
+      filterImageFromURL(image_url).then((response) => {
+        res.sendFile(response);
+        res.on('finish', function () {
+          deleteLocalFiles([response]);
         });
+      });
     }
-        else
-        res.status(404).send('Could not Succesfully Filter Image. Invalid URL');
-
+    else
+      res.status(404).send('Could not Succesfully Filter Image. Invalid URL');
   });
 
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT.\aws-elastic-beanstalk-cli-setup\scripts\bundled_installer
@@ -50,17 +47,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+
+  app.get('/filteredimage/', async (req, res) => {
+
+    let { image_url } = req.query;
+    let image = await filterImageFromURL(image_url);
+
+    if (image_url) {
+      console.log("Query worked");
+      filterImageFromURL(image_url).then((response) => {
+        res.sendFile(response);
+        res.on('finish', function () {
+          deleteLocalFiles([response]);
+        });
+      });
+    }
+    else
+      res.status(404).send('Could not Succesfully Filter Image. Invalid URL');
+  });
+
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
