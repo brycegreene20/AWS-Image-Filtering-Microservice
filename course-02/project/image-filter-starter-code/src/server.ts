8af24179525b22cd2674316ee82f51beb700de5f
Bryce Greene <brycegreene20@gmail.com>
2021-06-28 14:07:38 -0400

    TypeScript Variable Update

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -12,7 +12,7 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get('/filteredimage/', async (req, res) => {
+  app.get('/filteredimage/', async (req:express.Request, res:express.Response) => {
     let { image_url } = req.query;
     let image = await filterImageFromURL(image_url);
 
